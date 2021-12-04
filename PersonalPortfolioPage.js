63ecd13831314780072c6eee0bee58ef0e6f374c
Matt <91234966+mphDev84@users.noreply.github.com>
2021-12-04 14:27:25 -0500

    Create PersonalPortfolioPage.js

---
+++ PersonalPortfolioPage.js
@@ -0,0 +1,38 @@
+function light(){ document.getElementById("home").style.background='linear-gradient(to right, #F7F7F7 0%, #F4F4F4 50%, #E5E5E5 100%)';
+document.getElementById("header1").style.color='black';
+document.getElementById("para1").style.color='black';
+document.getElementById("para3").style.color='black';
+document.getElementById("contact2").style.color='black';
+document.getElementById("para4").style.color='black';
+ var y = document.getElementsByClassName("lightButt");
+  y[0].style.color='black';
+  y[1].style.color='black';
+var x =document.getElementsByClassName("button");
+  x[0].style.color='black';
+  x[1].style.color='black';
+  x[2].style.color='black';   
+var z =document.getElementsByClassName("nav-link");
+  z[0].style.color="black";  
+    z[1].style.color="black";
+    z[2].style.color="black"; 
+}
+function dark(){
+  document.getElementById("home").style.background='linear-gradient(to right, #141414 0%, #323232 50%, #636363 100%)';
+document.getElementById("header1").style.color='white';
+document.getElementById("para1").style.color='white';
+document.getElementById("para3").style.color='white';
+document.getElementById("contact2").style.color='white';
+document.getElementById("para4").style.color='#F7FFE6'; var x =document.getElementsByClassName("button");
+  x[0].style.color='#CCFFFF';
+  x[1].style.color='#CCFFFF';
+  x[2].style.color='#CCFFFF';
+   var y = document.getElementsByClassName("lightButt");
+  y[0].style.color='white';
+  y[1].style.color='white';
+  var z =document.getElementsByClassName("nav-link");
+  z[0].style.color="white";  
+    z[1].style.color="white";
+    z[2].style.color="white";  
+  
+
+}
